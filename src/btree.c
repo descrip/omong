02668bf1121bab479c5e26139db0bbdcdcbd97fd
Jeffrey Zhao <jezhao1999@gmail.com>
2017-10-27 17:34:06 -0400

    Remove orig code, add bnode ctor + dtor

--- src/btree.c
+++ src/btree.c
@@ -15,70 +15,84 @@
 const int ORDER = 340;
 
 struct BNode {
-    int32_t num_keys;
-    char *keys, *ids, *children;
+  void *map;
+  uint32_t *num_keys;
+  int32_t *keys;
+  uint32_t *ids;
+  uint32_t *children;
 };
 
 struct BTree {
-    struct BNode *root;
+  struct BNode *root;
 };
 
-struct BNode *bnode_init(char *map) {
-    struct BNode *bn = malloc(sizeof(struct BNode));
-    bn->num_keys    = *(int32_t*) map;
-    bn->keys        = map+4;
-    bn->ids         = map+4 + sizeof(int32_t)*ORDER;
-    bn->children    = map+4 + (sizeof(int32_t)*2)*ORDER;
-    return bn;
+struct BNode *bnode_init(int fd, uint32_t offset) {
+  struct BNode *bn = malloc(sizeof(struct BNode));
+
+  size_t page_size = sysconf(_SC_PAGE_SIZE);
+  bn->map = mmap(NULL, page_size, PROT_READ | PROT_WRITE,
+      MAP_SHARED, fd, offset);
+
+  bn->num_keys  = (uint32_t*) bn->map;
+  bn->keys      = (int32_t*) (bn->map+4);
+  bn->ids       = (uint32_t*) (bn->map+4 + sizeof(int32_t)*ORDER);
+  bn->children  = (uint32_t*)
+    (bn->map+4 + sizeof(int32_t)*ORDER + sizeof(uint32_t)*ORDER);
+
+  return bn;
 }
 
-void bnode_write_test(char *map) {
-    int num_keys = 2;
-    memcpy(map, &num_keys, sizeof(num_keys));
-    int32_t keys[] = {23, 57};
-    memcpy(map+4, keys, sizeof(keys));
-    int32_t ids[] = {1, 2};
-    memcpy(map+4 + sizeof(int32_t)*ORDER, ids, sizeof(ids));
+void bnode_destroy(struct BNode *bn) {
+  assert(munmap(bn->map, sizeof(int32_t)*2) == 0);
+}
+
+void bnode_write_test(void *map) {
+  int num_keys = 2;
+  memcpy(map, &num_keys, sizeof(num_keys));
+  int32_t keys[] = {23, 57};
+  memcpy(map+4, keys, sizeof(keys));
+  uint32_t ids[] = {1, 2};
+  memcpy(map+4 + sizeof(int32_t)*ORDER, ids, sizeof(ids));
 }
 
 int bnode_key_lower_bound(struct BNode *bn, int32_t key) {
-    // TODO: linear search for now, replace with binary
-    for (int i = 0; i < ORDER; ++i)
-        if (key <= *(int32_t*) (bn->keys+i*sizeof(int32_t)))
-            return i;
-    return bn->num_keys;
+  // TODO: linear search for now, replace with binary
+  for (size_t i = 0; i < *bn->num_keys; ++i)
+    if (key <= bn->keys[i])
+      return i;
+  return *bn->num_keys;
 }
 
-static int32_t bnode_find(BNode *bn, int key) {
-    int ind = bnode_key_lower_bound(bn, key);
-    if (key == *(int32_t*) (bn->keys + ind*sizeof(int32_t)))
-        return *(int32_t*) (bn->ids + ind*sizeof(int32_t));
-    else {
-        int child = *(int32_t*) (bn->children + ind*sizeof(int32_t));
-        if (child == 0) return -1;
-        else return bnode_find(child);
-    }
+/*
+static int32_t bnode_find(struct BNode *bn, int key) {
+  int ind = bnode_key_lower_bound(bn, key);
+  if (key == *(int32_t*) (bn->keys + ind*sizeof(int32_t)))
+    return *(int32_t*) (bn->ids + ind*sizeof(int32_t));
+  else {
+    int child = *(int32_t*) (bn->children + ind*sizeof(int32_t));
+    if (child == 0) return -1;
+    else return bnode_find(child);
+  }
 }
+*/
 
+/*
 int btree_find(BTree *bt, int key) {
 
 }
+*/
 
 int main() {
-    int fd = open("test.bin", O_RDWR);
-    int page_size = sysconf(_SC_PAGE_SIZE);
+  int fd = open("test.bin", O_RDWR);
 
-    char *map = mmap(NULL, page_size, PROT_READ | PROT_WRITE,
-            MAP_SHARED, fd, 0);
+  // bnode_write_test(map);
+  struct BNode *bn = bnode_init(fd, 0);
 
-    // bnode_write_test(map);
-    struct BNode *bn = bnode_init(map);
+  int ind = bnode_key_lower_bound(bn, 99);
+  printf("%d\n", ind);
 
-    int ind = bnode_key_lower_bound(bn, 22);
-    printf("%d\n", ind);
+  bnode_destroy(bn);
+  close(fd);
 
-    assert(munmap(map, sizeof(int32_t)*2) == 0);
-    close(fd);
-
-    return 0;
+  return 0;
 }
